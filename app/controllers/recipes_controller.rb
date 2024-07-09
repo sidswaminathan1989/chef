fdd1acf42feeaccf327220ef081fe4733597c82d
Sid Swaminathan <sidswaminathan@yahoo.com>
2024-07-09 08:06:57 -0500

    Complete User Story 2

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,26 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipes_path, notice: 'Recipe was successfully created!'
+    else
+      render :new
+    end
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(:ingredients, :steps, :chef_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get '/recipes', to: "recipes#index"
   get '/chefs', to: "chefs#index"
 
+  get '/recipes/new', to: "recipes#new"
+  get '/recipes/:id', to: "recipes#show"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
